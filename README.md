1a90c5d2f3901a727cfbf385dc370065fcac026d
Ibrahim Mahmud <89999103+prohacker69@users.noreply.github.com>
2022-01-25 01:17:53 +0000

    i think Im gonna try something...

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-Work of Ibrahim Mahmud
-All rigths reserved to Ibrahim Mahmud
+It takes a lot of effort to create something as magnifgicient as this; I mean think about it... not very many people spend their monday nights attempting different approaches to the perfect Calculator. Well neither did I!
+
+I decided to create an ordinary Calculator with problems.
 
 I think I have an ego problem...
 Anyways!
 
-Welcome to my Python Calculator.
+Welcome to my Python Calculator. (Probably has issues here n there)
 
 Regardless of if your the teacher marking this or just someone looking to copy n paste a coding masterpeice, I hope you enjoy every bit of this assignment as much as I did.
 
--- calc.py
+++ calc.py
@@ -1,30 +1,6 @@
 #Ibrahim Mahmud
 #Computer science 30
 #01/24/22
-def main():
-
-    print("This is merely a simple calculator which can add, subtract, divide, and multiply.")
-    a = float(input("Enter any number:"))
-    b = float(input("Enter any number:"))
-    #The two numbers being calculated
-
-    add = a+b
-    print("The sum is " + add)
-    #adds a and b values
-
-    sub = a-b
-    print("The difference is" + sub)
-    #takes the value of a and removes the b equivalent
-
-    div = a/b
-    print("The fraction is" + div)
-    #takes a and divides it by the value of b
-
-    mul = a*b
-    print("The product is" + mul)
-    #a multiplied by the value of b
-
-main()
 
 
 
